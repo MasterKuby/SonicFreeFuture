b7bff1c0cf556ab26ae761d3cc7100a58b30a898
MasterKuby <160546998+MasterKuby@users.noreply.github.com>
2025-02-23 00:42:02 +0000

    Initial
    project was started BEFORE repo so just syncing everything up!

---
+++ scenes/ui scenes/loading screen/loading_screen.gd
@@ -0,0 +1,16 @@
+extends Control
+@onready var loadingBar = $"Loading Bar"
+var bigstep = 0
+
+func _ready():
+	loadingBar.value = 0
+
+
+func _process(delta):
+	loadingBar.value += randf_range(0.1, 2.0)
+	bigstep += 1
+	if loadingBar.value > 1000: get_tree().change_scene_to_file("res://scenes/ui scenes/main menu/main_menu.tscn")
+	if bigstep == 300:
+		bigstep = 0
+		var tween = create_tween()
+		tween.tween_property(loadingBar, "value", loadingBar.value + 250, 1.5)  # Moves over 0.5 seconds
