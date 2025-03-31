ef5ad5210eb1de95c238bffff3c8e5c7d645de81
MasterKuby <160546998+MasterKuby@users.noreply.github.com>
2025-03-31 23:58:13 +0100

    Movement Fix: Variables Scales Correctly / bugfix
    Sonic's Speed variable has been slightly decreased for balancing. In sonic & tails' scripts, places where speed, jump velocity and gravity are used are multiplied by the Scale Factor of the Script's attached node.
    
    Sonic has a minimum speed requirement to switch to his sprinting animation, so now this speed requirement scales with the node's scale and with the speed. The target value (maximum speed) is multiplied by the scale factor. Does this make sense... I hope so! Same applies for tails & fly feature. Now, scene can be scaled as many times as necessary. Camera zoom adjusted to accomodate scale and speed. (3 -> 2 zoom).
    
    Reason the first few lines' SPEED and JUMP_VELOCITY aren't multiplied by scale.x to simplify things is because when I did that it didn't work as intended. I'm no pro, but I believe since the variable declaration isn't in any function, it only happens once at the scene's initiation or some shit like that - if its not broken, don't fix it.
    
    Also cleaned up Sonic & Tails script for parity.

--- scenes/character scenes/sonic/sonic.gd
+++ scenes/character scenes/sonic/sonic.gd
@@ -7,16 +7,13 @@ extends CharacterBody2D
 
 var character = "sonic"
 
-var SPEED: float = 250.0
+var SPEED: float = 200.0
 var JUMP_VELOCITY: float = -250.0
 
 var dashed: bool = false
 var direction: int = 1
 var jumping: bool = false
 
-func quickReset():
-	if Input.is_action_pressed("reset"):
-		get_tree().reload_current_scene()
 
 func _physics_process(delta):
 	debugModeCheck(delta)
@@ -34,29 +31,29 @@ func debugModeCheck(delta):
 	if GV.debugMode == true and GV.activeCharacter == character:
 		if Input.is_action_pressed("up"):
 			velocity.y = 0
-			position.y += -10
+			position.y += -5 * scale.x
 		if Input.is_action_pressed("down"):
-			position.y += 10
+			position.y += 5 * scale.x
 			velocity.y = 0
 		if Input.is_action_pressed("left"):
-			position.x += -10	
+			position.x += -5 * scale.x
 		if Input.is_action_pressed("right"):
-			position.x += 10
+			position.x += 5 * scale.x
 
 func playerControl(delta):
 	direction = Input.get_axis("a", "d") # set direction
 	if not GV.activeCharacter == character:
 		direction = 0
 	if direction: # move 👋 fuck you BITCH
-		velocity.x = lerp(velocity.x, direction*SPEED, 0.05)
+		velocity.x = lerp(velocity.x, direction*SPEED*scale.x, 0.025)
 	elif dashed == false:
-		velocity.x = lerp(velocity.x, 0.0, 0.2) # slow down when have no direction
+		velocity.x = lerp(velocity.x, 0.0, 0.1) # slow down when have no direction
 	elif dashed == true:
-		velocity.x = lerp(velocity.x, 0.0, 0.2)
+		velocity.x = lerp(velocity.x, 0.0, 0.1)
 
 func gravityCheck(delta):
 	if not is_on_floor() and GV.debugMode == false:
-		velocity += get_gravity() * delta / 2
+		velocity += get_gravity() * delta / 2*scale.x
 
 func jumpingFalling(delta):
 	# If hasn't dashed, player animation will be jumping when going up and falling when going down.
@@ -71,7 +68,7 @@ func jumpingFalling(delta):
 				sprite.play()
 	# If jump button and criteria is met, jump.
 	if Input.is_action_pressed("w") and is_on_floor() and GV.debugMode == false and GV.activeCharacter == character:
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY*scale.x
 
 func airDash(delta):
 	# After double tapped in direction, dash in said locked direction. (locked in player movement script)
@@ -80,7 +77,7 @@ func airDash(delta):
 			dashed = true
 			sprite.animation = "dash forward"
 			velocity.y = 0
-			velocity.x += 2000*direction
+			velocity.x += 1000*direction * scale.x
 		if dashed == false:
 			dashingTimer.start()
 	if Input.is_action_just_pressed("d") and GV.activeCharacter == character:
@@ -88,7 +85,7 @@ func airDash(delta):
 			dashed = true
 			sprite.animation = "dash forward"
 			velocity.y = 0
-			velocity.x += 2000*direction
+			velocity.x += 1000*direction * scale.x
 		if dashed == false:
 			dashingTimer.start()
 	if is_on_floor():
@@ -98,19 +95,19 @@ func spriteAndCameraFlip():
 	if GV.activeCharacter == character:
 		if direction == -1 and velocity.x != 0:
 			sprite.flip_h = true
-			phantomCamera.set_follow_offset(Vector2(-150, 0))
+			phantomCamera.set_follow_offset(Vector2(-75 * scale.x, 0))
 		elif direction == 1 and velocity.x != 0:
 			sprite.flip_h = false
-			phantomCamera.set_follow_offset(Vector2(150, 0))
+			phantomCamera.set_follow_offset(Vector2(75 * scale.x, 0))
 
 # what abs() does is it removes the negative, so when we're moving to the left (direction becomes negative so velocity is negative) the function still works as intended since abs() removes the negative. We could also make 2 different ifs, one for if direction is 1 (right) and one for if direction is -1 (left)
 func animations():
 	if direction != 0 and is_on_floor() and !Input.is_action_pressed("w"):
 		if not sprite.animation == "run" or "sprint":
-			if abs(velocity.x) >= 400: 
+			if abs(velocity.x) >= 160*scale.x: 
 				sprite.animation = "sprint"
 				sprite.play()
-			if abs(velocity.x) <= 400: 
+			if abs(velocity.x) <= 160*scale.x: 
 				sprite.animation = "run"
 				sprite.play()
 	elif is_on_floor() and direction == 0:
@@ -121,3 +118,7 @@ func animations():
 func zIndexSort():
 	if GV.activeCharacter == character:if z_index != 1: set_z_index(1)
 	if GV.activeCharacter != character:if z_index != 0: set_z_index(0)
+
+func quickReset():
+	if Input.is_action_pressed("reset"):
+		get_tree().reload_current_scene()
--- scenes/character scenes/tails/tails.gd
+++ scenes/character scenes/tails/tails.gd
@@ -25,26 +25,27 @@ func _physics_process(delta):
 	animations()
 	move_and_slide()
 	zIndexSort()
+	quickReset()
 
 func debugModeCheck(delta):
 	if GV.debugMode == true and GV.activeCharacter == character:
 		if Input.is_action_pressed("up"):
 			velocity.y = 0
-			position.y += -5
+			position.y += -5 * scale.x
 		if Input.is_action_pressed("down"):
-			position.y += 5
+			position.y += 5 * scale.x
 			velocity.y = 0
 		if Input.is_action_pressed("left"):
-			position.x += -5	
+			position.x += -5 * scale.x
 		if Input.is_action_pressed("right"):
-			position.x += 5
+			position.x += 5 * scale.x
 
 func playerControl(delta):
 	direction = Input.get_axis("a", "d") # set direction
 	if not GV.activeCharacter == character:
 		direction = 0
 	if direction: # move
-		velocity.x = lerp(velocity.x, direction*SPEED, 0.025)
+		velocity.x = lerp(velocity.x, direction*SPEED*scale.x, 0.025)
 	elif dashed == false:
 		velocity.x = lerp(velocity.x, 0.0, 0.1) # slow down when have no direction
 	elif dashed == true:
@@ -52,9 +53,9 @@ func playerControl(delta):
 
 func gravityCheck(delta):
 	if not is_on_floor() and GV.debugMode == false:
-		velocity += (get_gravity() * delta) / 4
+		velocity += (get_gravity() * delta) / 4*scale.x
 	if not is_on_floor() and GV.debugMode == false and flyingEnabled:
-		velocity += (get_gravity() * delta) / 16
+		velocity += (get_gravity() * delta) / 16*scale.x
 
 func jumpingFallingFlying(delta):
 	# If hasn't dashed, player animation will be jumping when going up and falling when going down.
@@ -69,9 +70,9 @@ func jumpingFallingFlying(delta):
 				sprite.play()
 	# If jump button and criteria is met, jump.
 	if Input.is_action_pressed("w") and is_on_floor() and GV.debugMode == false and GV.activeCharacter == character:
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY*scale.x
 	if Input.is_action_pressed("w") and !is_on_floor() and GV.debugMode == false and GV.activeCharacter == character and flyingEnabled: #if flying then fly
-		velocity.y = JUMP_VELOCITY/3
+		velocity.y = JUMP_VELOCITY/3*scale.x
 	if Input.is_action_just_pressed("f") and GV.activeCharacter == "tails": # toggle flying with f key
 		if flyingEnabled:
 			flyingEnabled = false
@@ -90,7 +91,7 @@ func airDash(delta):
 			dashed = true
 			sprite.animation = "dash forward"
 			velocity.y = 0
-			velocity.x += 1000*direction
+			velocity.x += 1000*direction * scale.x # Tails dash is intentionally weaker than Sonic's
 		if dashed == false:
 			dashingTimer.start()
 	if Input.is_action_just_pressed("d") and GV.activeCharacter == character:
@@ -98,7 +99,7 @@ func airDash(delta):
 			dashed = true
 			sprite.animation = "dash forward"
 			velocity.y = 0
-			velocity.x += 1000*direction
+			velocity.x += 1000*direction * scale.x
 		if dashed == false:
 			dashingTimer.start()
 	if is_on_floor():
@@ -108,18 +109,18 @@ func spriteAndCameraFlip():
 	if GV.activeCharacter == character:
 		if direction == -1 and velocity.x != 0:
 			sprite.flip_h = true
-			phantomCamera.set_follow_offset(Vector2(-75, 0))
+			phantomCamera.set_follow_offset(Vector2(-75 * scale.x, 0))
 		elif direction == 1 and velocity.x != 0:
 			sprite.flip_h = false
-			phantomCamera.set_follow_offset(Vector2(75, 0))
+			phantomCamera.set_follow_offset(Vector2(75 * scale.x, 0))
 
 func animations():
 	if direction != 0 and is_on_floor() and !Input.is_action_pressed("w"):
 		if not sprite.animation == "run":
-			if abs(velocity.x) >= 200: 
+			if abs(velocity.x) >= 200*scale.x: 
 				sprite.animation = "run"
 				sprite.play()
-			if abs(velocity.x) <= 200: 
+			if abs(velocity.x) <= 200*scale.x: 
 				sprite.animation = "run"
 				sprite.play()
 	elif is_on_floor() and direction == 0:
@@ -130,3 +131,7 @@ func animations():
 func zIndexSort():
 	if GV.activeCharacter == character:if z_index != 1: set_z_index(1)
 	if GV.activeCharacter != character:if z_index != 0: set_z_index(0)
+
+func quickReset():
+	if Input.is_action_pressed("reset"):
+		get_tree().reload_current_scene()
