0a0e265f85fa82b9f1701a67c7b78af5544c0638
MasterKuby <160546998+MasterKuby@users.noreply.github.com>
2025-04-08 13:41:03 +0100

    LimboConsole, 2 Console Commands
    Added LimboConsole plugin/addon and added to commands for demonstration and debugging purposes, "health" and "debugging". Health command takes 2 parameters, first parameter is for the character's health you want to modify and the second parameter is for the value you want to modify it by.
    
    Ideally, we'd have a command called "stat" or "setstat" or "set" or something which could go like this:
    
    set stat hp sonic 50
    which would set sonic's HP stat to 50:
    or we could just keep it simple with commands like
    
    hp sonic 50
    or
    sethp sonic 50
    I think that's better.
    
    "Debugging" command just takes one parameter, a boolean. If true, debugMode is activated and vice versa.
    Could also add a command for a debugMenu which could do the same thing as the commands but with a visual interface.

--- scenes/scripts/GV.gd
+++ scenes/scripts/GV.gd
@@ -13,16 +13,23 @@ func _process(delta):
 			activeCharacter = "tails"
 		elif activeCharacter == "tails":
 			activeCharacter = "sonic"
-	if Input.is_action_just_pressed("`"):
-		if debugMode == false:
-			debugMode = true
-			print("Debug Mode Activated")
-		elif debugMode == true:
-			debugMode = false
-			print("Debug Mode Deactivated")
 
 func _ready():
 	DiscordRPC.app_id = 1341078725323919490
 	DiscordRPC.large_image = "Sonic: Free Future"
 	print("Discord working: " + str(DiscordRPC.get_is_discord_working()))
 	DiscordRPC.refresh()
+	# Commands
+	LimboConsole.register_command(debugging)
+	 # This registers the command, the first param is the callable command.
+	LimboConsole.register_command(health)
+	
+func debugging(state: bool):
+	debugMode = state
+	print("debugging is", state)
+
+func health(character: String, value: int):
+	if character == "sonic":
+		sonicHealth+= value
+	if character == "tails":
+		tailsHealth+= value
