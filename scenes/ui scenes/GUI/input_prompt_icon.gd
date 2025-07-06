d04bdf7596755ab1c0394167e7555617ca4ce139
MasterKuby <160546998+MasterKuby@users.noreply.github.com>
2025-07-06 12:43:33 +0100

    input prompts addon
    Added Kenney's Input Prompts assets

---
+++ scenes/ui scenes/GUI/input_prompt_icon.gd
@@ -0,0 +1,14 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	# later here add a connecting signal which shows/hides the interact 
+	# icon or changes the animation when an interaction area is entered. 
+	# Make the character selected emit a signal when interaction area is entered.
